c2f88847e54d4ea50482ac6295fe072afa64a5a1
Jorge Anais <jorgeanais@MacPro.local>
2024-08-15 21:56:47 -0400

    Populate Image and Target tables and update Image.path if correspond

--- project/app.py
+++ project/app.py
@@ -1,60 +1,76 @@
 import datetime
+from pathlib import Path
+
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 from .database import create_db_and_tables, engine
 from .models import Image, Result, Run, Parameter, Target
+from .populate import list_images
 
 
-def create_images():
+def create_test_images():
     with Session(engine) as session:
         target_minni114 = Target(name="Minni114", ra=280.0375, dec=-30.5739)
         target_minni666 = Target(name="Minni666", ra=66.66, dec=-66.66)
         session.add(target_minni114)
         session.add(target_minni666)
         session.commit()
-        # session.refresh(target_minni114)
         
         image_test_1 = Image(
-            name="ifb444010_drc.fits",
+            filename="ifb444010_drc.fits",
             type="drc",
-            path="Minni144_reccomendedfile/MAST_2024-07-16T15_54_37.493Z/HST/ifb444020_drc.fits",
+            path="MAST_2024-07-16T15_54_37.493Z/HST/ifb444020_drc.fits",
             exptime=1040,
             band="F606W",
             dateobs=datetime.datetime.fromisoformat("2024-03-07T23:51:40+00:00"),
-            target_id=target_minni114.id
+            target_id=target_minni114.id,
         )
         image_test_2 = Image(
-            name="ifb444010_drz.fits",
+            filename="ifb444010_drz.fits",
             type="drz",
-            path="Minni144_reccomendedfile/MAST_2024-07-16T15_54_37.493Z/HST/ifb444020_drz.fits",
+            path="MAST_2024-07-16T15_54_37.493Z/HST/ifb444020_drz.fits",
             exptime=1040,
             band="F606W",
             dateobs=datetime.datetime.fromisoformat("2024-03-07T23:51:40+00:00"),
-            target_id=target_minni114.id
+            target_id=target_minni114.id,
         )
         session.add(image_test_1)
         session.add(image_test_2)
         session.commit()
 
-def select_images():
-    with Session(engine) as session:
-        statement = select(Image, Target).where(Image.target_id == Target.id)
-        results = session.exec(statement)
-        for im, target in results:
-            print("Image:", im, "Target:", target)
 
-def select_images_alt1():
+# Check if image exists in the database
+def check_image_exists(image: Image):
     with Session(engine) as session:
-        statement = select(Image, Target).join(Target)
-        results = session.exec(statement)
-        for im, target in results:
-            print("Image:", im, "Target:", target)
+        statement = select(Image).where(Image.filename == image.filename)
+        result = session.exec(statement)
+        return result.one_or_none()
+
+
+def populate_db_with_images():
+    BASE_DATA_PATH = (
+        "/Users/jorgeanais/Documents/data/HST"  # "/home/jorge/Documents/data/hst_data"
+    )
+    data_path = Path(BASE_DATA_PATH)
+    images = list_images(data_path)
+    
+    with Session(engine) as session:
+        for image in images:
+            db_image = check_image_exists(image)
+            if db_image is None:
+                session.add(image)
+            # Update path if it has changed
+            elif db_image.path != image.path:
+                db_image.path = image.path
+                session.add(db_image)
+        
+        session.commit()
 
 
 def main():
     create_db_and_tables()
-    create_images()
-    select_images_alt1()
+    # create_test_images()
+    populate_db_with_images()
 
 
 if __name__ == "__main__":
---
+++ project/populate.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+import datetime
+
+from astropy.io import fits
+import pandas as pd
+
+from .models import Image, Target
+
+
+def read_fits_header(fits_path: Path) -> dict:
+    with fits.open(fits_path) as hdul:
+        header = hdul[0].header
+    
+    header_selection = {
+        "filename": fits_path.name,
+        "type": fits_path.name[-8:-5],
+        "path": str(fits_path.parent),
+        "dateobs": (
+            datetime.datetime.fromisoformat(
+                header["DATE-OBS"] + "T" + header["TIME-OBS"]
+            )
+            if "DATE-OBS" in header and "TIME-OBS" in header
+            else None
+        ),
+        "band": header["FILTER"] if "FILTER" in header else None,
+        "exptime": header["EXPTIME"] if "EXPTIME" in header else None,
+        "name": header["TARGNAME"],
+        "ra": header["RA_TARG"],
+        "dec": header["DEC_TARG"],
+        "detector": header["DETECTOR"],
+    }
+    
+    return header_selection
+
+
+def build_table(fits_paths: list[Path]) -> pd.DataFrame:
+    headers = []
+    for fits_path in fits_paths:
+        header = read_fits_header(fits_path)
+        headers.append(header)
+    df = pd.DataFrame(headers)
+    return df
+
+
+def get_targets(df: pd.DataFrame) -> dict[str, Target]:
+    df_unique_targets = df[["name", "ra", "dec"]].drop_duplicates()
+    targets_dict = {}
+    for index, row in df_unique_targets.iterrows():
+        row_dict = row.to_dict()
+        target = Target(**row_dict)
+        targets_dict = {target.name: target}
+    
+    return targets_dict
+
+
+def get_images(df: pd.DataFrame) -> list[Image]:
+    targets_dict = get_targets(df)
+    
+    df_images = df[["filename", "type", "path", "exptime", "band", "dateobs", "name"]]
+    output_list = []
+
+    for _, row in df_images.iterrows():
+        row_dict = row.to_dict()
+        target_name = row_dict.pop("name")
+        row_dict["target"] = targets_dict[target_name]
+        # Convert NaT to None
+        if row_dict["dateobs"] is pd.NaT:
+            row_dict["dateobs"] = None
+        image = Image(**row_dict)
+        output_list.append(image)
+    
+    return output_list
+
+
+def list_images(data_path: Path) -> list[Image]:
+    fits_paths = list(data_path.glob("**/*.fits"))
+    df = build_table(fits_paths)
+    images = get_images(df)
+    return images
